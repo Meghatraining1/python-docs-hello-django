f68fa14b40cc60c6bfdd6e8383c64ae880feeeb0
Meghatraining1 <83501845+Meghatraining1@users.noreply.github.com>
2022-02-01 22:09:10 +0530

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello(request):
-    return HttpResponse("Congratulations! You have successfully created your we apps!")
+    return HttpResponse("Congratulations! You have successfully created your web apps!")
